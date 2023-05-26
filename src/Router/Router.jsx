a2d7284b54988c8338e7eaa89b39bfd16d0399d0
TariqulIslamTusher <islamtariqul652@gmail.com>
2023-05-26 23:13:46 +0600

    First commit

---
+++ src/Router/Router.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayouts from '../Layouts/MainLayouts';
+import Home from '../Pages/Home/Home';
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayouts></MainLayouts>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        }
+      ]
+    },
+  ]);
+
+export default router;
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import router from './Router/Router.jsx';
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <div className="max-w-5xl mx-auto">
+      <RouterProvider router={router} />
+    </div>
+  </React.StrictMode>
+);
